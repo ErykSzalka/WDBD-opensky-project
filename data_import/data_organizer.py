7c2ed3f7c7946829ef4a1fa98f18764fc4bbbda9
Jan Tchurzewski <jtchurzewski@student.agh.edu.pl>
2026-06-16 17:54:10 +0200

    clean up code

--- data_import/data_organizer.py
+++ data_import/data_organizer.py
@@ -41,38 +41,32 @@ class Arrival:
 
 @dataclass
 class RadarState:
-    icao24: str                     # [0] Unikalny kod samolotu
-    callsign: Optional[str]         # [1] Znak wywoławczy (np. LOT123)
-    origin_country: str             # [2] Kraj pochodzenia samolotu
-    time_position: Optional[int]    # [3] Czas ostatniej aktualizacji pozycji (Unix timestamp)
-    last_contact: int               # [4] Czas ostatniej jakiejkolwiek wiadomości (Unix)
-    longitude: Optional[float]      # [5] Długość geograficzna
-    latitude: Optional[float]       # [6] Szerokość geograficzna
-    baro_altitude: Optional[float]  # [7] Wysokość barometryczna (w metrach)
-    on_ground: bool                 # [8] Czy samolot jest na ziemi
-    velocity: Optional[float]       # [9] Prędkość względem ziemi (m/s)
-    true_track: Optional[float]     # [10] Kąt toru lotu (0-360 stopni)
-    vertical_rate: Optional[float]  # [11] Prędkość wznoszenia/opadania (m/s)
-    sensors: Optional[List[int]]    # [12] ID sensorów (zazwyczaj null dla darmowego konta)
-    geo_altitude: Optional[float]   # [13] Wysokość geometryczna (metry)
-    squawk: Optional[str]           # [14] Kod transpondera
-    spi: bool                       # [15] Wskaźnik specjalnego przeznaczenia (Special Purpose Indicator)
-    position_source: int            # [16] Źródło pozycji (0 = ADS-B, 1 = ASTERIX, 2 = MLAT, 3 = FLARM)
+    icao24: str               
+    callsign: Optional[str]         
+    origin_country: str            
+    time_position: Optional[int]    
+    last_contact: int               
+    longitude: Optional[float]      
+    latitude: Optional[float]       
+    baro_altitude: Optional[float]  
+    on_ground: bool                
+    velocity: Optional[float]       
+    true_track: Optional[float]     
+    vertical_rate: Optional[float]  
+    sensors: Optional[List[int]]    
+    geo_altitude: Optional[float]   
+    squawk: Optional[str]           
+    spi: bool                       
+    position_source: int            
 
     @classmethod
     def from_api_list(cls, data: list) -> 'RadarState':
-        """
-        Tworzy obiekt RadarState na podstawie surowej listy zwróconej przez /states/all.
-        """
-        # Czyścimy callsign tak samo jak w klasie Arrival
+
         clean_callsign = data[1]
         if clean_callsign and isinstance(clean_callsign, str):
             clean_callsign = clean_callsign.strip()
 
-        # Aby uniknąć błędu IndexError, w przypadku gdyby API nagle zwróciło krótszą listę
-        # upewniamy się, że mamy przynajmniej 17 elementów (co jest standardem OpenSky).
         if len(data) < 17:
-            # Uzupełnia brakujące elementy wartościami None
             data.extend([None] * (17 - len(data)))
 
         return cls(
